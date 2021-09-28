da0eea4cc8565d059cfe6fff7e66983b2b33b97b
Alexander <alexander24yt1@gmail.com>
2021-09-28 10:11:37 +0300

    Update install_ruby.sh

--- configs-cripts/install_ruby.sh
+++ configs-cripts/install_ruby.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
-ruby -v
-bundler -v
+apt update
+apt install -y ruby-full ruby-bundler build-essential
