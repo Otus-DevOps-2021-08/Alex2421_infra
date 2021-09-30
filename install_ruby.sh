b724f49ff0fc19a8d40f415260ecdb13dd31cbac
Alexander <90255754+Alex2421@users.noreply.github.com>
2021-09-30 13:35:44 +0300

    Update install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-sudo apt update
-sudo apt install -y ruby-full ruby-bundler build-essential
-ruby -v
-bundler -v
+apt-get --assume-yes update
+apt-get --assume-yes install ruby-full ruby-bundler build-essential
