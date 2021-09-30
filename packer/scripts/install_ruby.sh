f15c26bfb691ebe21c20ef0265796920aae795b1
Alexander <alexander24yt1@gmail.com>
2021-09-30 12:54:57 +0300

    Update install_ruby.sh

--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
 apt-get update
-apt install -y ruby-full ruby-bundler build-essential
+echo "sleep 2m for install updates"; sleep 2m; echo "start install ruby"
+DEBIAN_FRONTEND=noninteractive apt-get install -y ruby-full ruby-bundler build-essential git
