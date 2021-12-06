e384af44aabf01f6d9e3d2b70802c424f7f120af
Alexander <you@example.com>
2021-12-06 16:36:23 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,12 +13,17 @@ variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
-variable image_id {
-  description = "Disk image"
-}
-variable subnet_id{
-  description = "Subnet"
-}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+  }
+
+variable db_disk_image {
+    description = "Disk image for reddit db"
+    default = "reddit-db-base"
+  }
+
 variable service_account_key_file{
   description = "key .json"
 }
